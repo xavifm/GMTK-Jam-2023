337cd4a1a1c856775b7bca1ceb8abadb2b0eac09
xavifm <60983549+xavifm@users.noreply.github.com>
2023-07-08 18:30:28 +0200

    finished the npc system

--- Fro-offense/Assets/Scripts/Map Elements/Vehicles/Car.cs
+++ Fro-offense/Assets/Scripts/Map Elements/Vehicles/Car.cs
@@ -2,6 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : Element
+public class Car : Element
 {
+    float moveTimer = 0;
+    int carxDIR = 1;
+
+    const float TIMER_BASE = 2;
+
+    private void Start()
+    {
+        base.Start();
+    }
+
+    private void Update()
+    {
+        base.Update();
+        MoveStateMachine();
+    }
+
+    void MoveStateMachine()
+    {
+        moveTimer -= Time.deltaTime;
+
+        if (moveTimer <= 0)
+        {
+            moveTimer = TIMER_BASE;
+            MapSystem.SquareValue nextSquareValue = GetNextSquare((int)moveSystem.destinationVector.x + carxDIR, (int)moveSystem.destinationVector.z);
+            Debug.Log(nextSquareValue.ToString());
+
+            switch (nextSquareValue)
+            {
+                case MapSystem.SquareValue.EMPTY:
+                    moveSystem.destinationVector = new Vector3(moveSystem.destinationVector.x + carxDIR, moveSystem.destinationVector.y, moveSystem.destinationVector.z);
+                    break;
+                case MapSystem.SquareValue.ANIMAL:
+                    moveSystem.destinationVector = new Vector3(moveSystem.destinationVector.x + carxDIR, moveSystem.destinationVector.y, moveSystem.destinationVector.z);
+                    break;
+                case MapSystem.SquareValue.OBSTACLE:
+                    SwitchCarDir();
+                    break;
+                case MapSystem.SquareValue.HOLE:
+                    SwitchCarDir();
+                    break;
+                case MapSystem.SquareValue.OUTSIDE_MAP:
+                    SwitchCarDir();
+                    break;
+            }
+        }
+    }
+
+    void SwitchCarDir()
+    {
+        if (carxDIR == 1)
+            carxDIR = -1;
+        else if (carxDIR == -1)
+            carxDIR = 1;
+    }
+
+    MapSystem.SquareValue GetNextSquare(int x, int y)
+    {
+        return map.GetSquareValue(x, y);
+    }
 }
