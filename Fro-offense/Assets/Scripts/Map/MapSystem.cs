40e4e476e3b3e0de697dc4ed244e03029d0dae7f
xavifm <60983549+xavifm@users.noreply.github.com>
2023-07-08 13:35:26 +0200

    created the new board system

---
+++ Fro-offense/Assets/Scripts/Map/MapSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSystem : MonoBehaviour
+{
+    public enum SquareValue { EMPTY, ANIMAL, CAR, OBSTACLE, OUTSIDE_MAP }
+
+    [SerializeField] int width;
+    [SerializeField] int height;
+
+    internal SquareValue[][] squareMatrix;
+
+    void Start()
+    {
+        InitMatrix();
+        squareMatrix[0][0] = SquareValue.ANIMAL;
+    }
+
+    void InitMatrix()
+    {
+        squareMatrix = new SquareValue[width][];
+
+        for (int index = 0; index < width; index++)
+        {
+            squareMatrix[index] = new SquareValue[height];
+
+            for (int index2 = 0; index2 < height; index2++)
+                squareMatrix[index][index2] = SquareValue.EMPTY;
+        }
+    }
+
+    public void SetSquareValue(int x, int y, SquareValue value)
+    {
+        squareMatrix[x][y] = value;
+    }
+
+    public SquareValue GetSquareValue(int x, int y)
+    {
+        if((x >= 0 && x < width) && (y >= 0 && y < height))
+            return squareMatrix[x][y];
+
+        return SquareValue.OUTSIDE_MAP;
+    }
+}
---
+++ Fro-offense/Assets/Scripts/Map/MouseSystem.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSystem : MonoBehaviour
+{
+    Plane plane = new Plane(Vector3.up, RAYCAST_POSY);
+    Vector3 worldPosition = Vector3.zero;
+    Camera cameraMain;
+
+    const float RAYCAST_POSY = -0.1f;
+    const float LERP_SPEED = 20;
+    const float COLOR_MARK_ALPHA = 0.3f;
+
+    MapSystem map;
+
+    [SerializeField] Material colorMat;
+
+    private void Start()
+    {
+        map = GameObject.FindGameObjectWithTag("Map").GetComponent<MapSystem>();
+        cameraMain = Camera.main;
+    }
+
+    void Update()
+    {
+        SetMarkPositionWithRaycast();
+        CheckMapTile();
+    }
+
+    void SetMarkPositionWithRaycast()
+    {
+        float distance;
+        Ray ray = cameraMain.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out distance))
+            worldPosition = ray.GetPoint(distance);
+
+        worldPosition = new Vector3(Mathf.Round(worldPosition.x), worldPosition.y, Mathf.Round(worldPosition.z));
+        transform.position = Vector3.Lerp(transform.position, worldPosition, Time.deltaTime * LERP_SPEED);
+    }
+
+    void CheckMapTile()
+    {
+        MapSystem.SquareValue tileValue = map.GetSquareValue((int)worldPosition.x, (int)worldPosition.z);
+
+        if (tileValue.Equals(MapSystem.SquareValue.ANIMAL) || tileValue.Equals(MapSystem.SquareValue.OBSTACLE) || tileValue.Equals(MapSystem.SquareValue.OUTSIDE_MAP))
+            colorMat.color = Color.red;
+        else
+            colorMat.color = Color.green;
+
+        Color newColor = colorMat.color;
+        newColor.a = COLOR_MARK_ALPHA;
+
+        colorMat.color = newColor;
+
+        Debug.Log(map.GetSquareValue((int) worldPosition.x, (int) worldPosition.z).ToString());
+    }
+}
